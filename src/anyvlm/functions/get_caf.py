130c8c6f3f05354e1d8c868f105f080c9d3eba97
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-12-04 13:33:17 -0500

    fix casing in function params

--- src/anyvlm/functions/get_caf.py
+++ src/anyvlm/functions/get_caf.py
@@ -6,16 +6,21 @@ from anyvlm.anyvar.base_client import BaseAnyVarClient
 
 
 def get_caf(
-    anyvar_client: BaseAnyVarClient, assembly_id: str, referenceName: str, start: int, referenceBases: str, alternateBases: str
+    anyvar_client: BaseAnyVarClient,
+    assembly_id: str,
+    reference_name: str,
+    start: int,
+    reference_bases: str,
+    alternate_bases: str,
 ) -> list[CohortAlleleFrequencyStudyResult]:
     """Retrieve Cohort Allele Frequency data for all known variants matching provided search params
 
     :param anyvar_client: AnyVar client
     :param assembly_id: The reference assembly to utilize - must be one of: "GRCh37", "GRCh38", "hg38", "hg19"
-    :param referenceName: The chromosome to search on, with an optional "chr" prefix - e.g., "1", "chr22", "X", "chrY", etc.
+    :param reference_name: The chromosome to search on, with an optional "chr" prefix - e.g., "1", "chr22", "X", "chrY", etc.
     :param start: start of range search
-    :param referenceBases: Genomic bases ('T', 'AC', etc.)
-    :param alternateBases: Genomic bases ('T', 'AC', etc.)
+    :param reference_bases: Genomic bases ('T', 'AC', etc.)
+    :param alternate_bases: Genomic bases ('T', 'AC', etc.)
     :return: list of CAFs contained in search interval
     """
-    raise NotImplementedError #TODO: Implement this. See Issue #16.
+    raise NotImplementedError  # TODO: Implement this. See Issue #16.
