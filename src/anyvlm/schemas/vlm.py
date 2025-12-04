355157f1936c35cbbdd1e2908d67b61f47ad7bd6
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-12-04 13:34:20 -0500

    add newline to end of file

--- src/anyvlm/schemas/vlm.py
+++ src/anyvlm/schemas/vlm.py
@@ -1,7 +1,9 @@
 """Schemas relating to VLM API."""
+
 from pydantic import BaseModel
 
 
 class VlmResponse(BaseModel):
     """Define response structure for the vlm-query endpoint."""
+
     # TODO: Fill this in. See Issue #13
