f354a34d8b8430acc4bea032721f69eea35e5d94
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-12-05 13:17:27 -0500

    update zygosity getter func to raise a NotImplementedError

--- src/anyvlm/functions/build_vlm_response.py
+++ src/anyvlm/functions/build_vlm_response.py
@@ -1,6 +1,5 @@
 """Craft a VlmResponse object from a list of CohortAlleleFrequencyStudyResults"""
 
-from ga4gh.core.models import MappableConcept
 from ga4gh.va_spec.base.core import CohortAlleleFrequencyStudyResult
 
 from anyvlm.schemas.vlm import (
@@ -13,38 +12,15 @@ from anyvlm.schemas.vlm import (
 from anyvlm.utils.types import Zygosity
 
 
-def _extract_zygosity(caf_study_result: CohortAlleleFrequencyStudyResult) -> Zygosity:
+def _get_caf_zygosity() -> Zygosity:
     """Extracts a zygosity from a CohortAlleleFrequencyStudyResult.
-    #TODO: This is my best guess at how we'll want to represent zygosity in our CAF results. We may need
-    to update this function during or after Issue #16 based on how we choose to implement our CAF data pull.
+    #TODO: We will need to implement this function during or after Issue #16 based on how we choose to
+    store CAF data/implement the CAF data pull.
 
     :param caf_study_result: The CohortAlleleFrequencyStudyResult whose zygosity we wish to determine.
     :return: The `Zygosity` of the study result.
     """
-    cohort_characteristics: list[MappableConcept] | None = (
-        caf_study_result.cohort.characteristics
-    )
-    if not (cohort_characteristics):
-        error_message: str = "Each CohortAlleleFrequencyStudyResult's 'cohort' field must have 'characteristics' set"
-        raise ValueError(error_message)
-
-    zygosity: str | None = next(
-        (
-            concept.name
-            for concept in cohort_characteristics
-            if getattr(concept, "conceptType", None) == "Zygosity"
-        ),
-        None,
-    )
-    if not zygosity:
-        error_message: str = "'CohortAlleleFrequencyStudyResult.cohort.characteristics' must contain a 'Zygosity' entry"
-        raise ValueError(error_message)
-
-    try:
-        return Zygosity(zygosity)
-    except ValueError as e:
-        error_message: str = f"Invalid zygosity provided: {zygosity}"
-        raise ValueError(error_message) from e
+    raise NotImplementedError
 
 
 def build_vlm_response(caf_data: list[CohortAlleleFrequencyStudyResult]) -> VlmResponse:
@@ -67,7 +43,7 @@ def build_vlm_response(caf_data: list[CohortAlleleFrequencyStudyResult]) -> VlmR
                         exists=True,
                         # TODO - HandoverType.id represents the ID of the node from which the dataset was pulled.
                         # In the future, this ID should be set dynamically.
-                        id=f"{HandoverType.id} {_extract_zygosity(caf_study_result)}",
+                        id=f"{HandoverType.id} {_get_caf_zygosity()}",
                         resultsCount=caf_study_result.focusAlleleCount,
                     )
                 ]
