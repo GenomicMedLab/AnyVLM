dd564941cd5221da8be05cc24fb811f9ce36807f
Kori Kuzma <korikuzma@gmail.com>
2025-12-24 16:35:54 -0500

    feat: add anyvlm database (#28)
    close #23
    
    * Started some docs work for #25

---
+++ src/anyvlm/storage/postgres.py
@@ -0,0 +1,70 @@
+"""Provide PostgreSQL-based storage implementation."""
+
+from urllib.parse import urlparse
+
+from ga4gh.va_spec.base import CohortAlleleFrequencyStudyResult
+from sqlalchemy import create_engine, delete
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import sessionmaker
+
+from anyvlm.storage import orm
+from anyvlm.storage.base_storage import (
+    Storage,
+)
+from anyvlm.storage.mapper_registry import mapper_registry
+from anyvlm.storage.orm import create_tables
+
+
+class PostgresObjectStore(Storage):
+    """PostgreSQL storage backend using dedicated ORM tables."""
+
+    MAX_ROWS = 100
+
+    def __init__(self, db_url: str, *args, **kwargs) -> None:
+        """Initialize PostgreSQL storage.
+
+        :param db_url: Database connection URL (e.g., postgresql://user:pass@host:port/db)
+        """
+        self.db_url = db_url
+        self.engine = create_engine(db_url)
+        self.session_factory = sessionmaker(bind=self.engine)
+        create_tables(self.db_url)
+
+    def close(self) -> None:
+        """Close the storage backend."""
+
+    def wipe_db(self) -> None:
+        """Wipe all data from the storage backend."""
+        with self.session_factory() as session, session.begin():
+            session.execute(delete(orm.AlleleFrequencyData))
+
+    @property
+    def sanitized_url(self) -> str:
+        """Return a sanitized URL (password masked) of the database connection string."""
+        parsed = urlparse(self.db_url)
+        netloc = ""
+        if parsed.username:
+            netloc += parsed.username
+            if parsed.password:
+                netloc += ":****"
+            netloc += "@"
+        if parsed.hostname:
+            netloc += f"{parsed.hostname}"
+        if parsed.port:
+            netloc += f":{parsed.port}"
+        return f"{parsed.scheme}://{netloc}{parsed.path}"
+
+    def add_allele_frequencies(self, caf: CohortAlleleFrequencyStudyResult) -> None:
+        """Add allele frequency data to the database. Will skip conflicts.
+
+        NOTE: For now, this will only insert a single caf record into the database.
+        Single insertion is used to do a simple test of the storage backend.
+        Issue-34 will support batch insertion of caf records.
+
+        :param caf: Cohort allele frequency study result object to insert into the DB
+        """
+        db_entity = mapper_registry.to_db_entity(caf)
+        stmt = insert(orm.AlleleFrequencyData).on_conflict_do_nothing()
+
+        with self.session_factory() as session, session.begin():
+            session.execute(stmt, db_entity.to_dict())
