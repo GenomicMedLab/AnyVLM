5054851d00a00167bd17060a1b69e81dafb50ace
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-02-09 22:33:00 -0500

    cleanup

--- tests/unit/functions/test_build_vlm_response.py
+++ tests/unit/functions/test_build_vlm_response.py
@@ -7,13 +7,28 @@ from anyvlm.functions.build_vlm_response import (
     build_vlm_response,
 )
 from anyvlm.schemas.vlm import ResponseSummary, ResultSet, VlmResponse
-from anyvlm.utils.funcs import sum_nullables
 from anyvlm.utils.types import (
     AncillaryResults,
     AnyVlmCohortAlleleFrequencyResult,
     QualityMeasures,
     Zygosity,
 )
+
+
+def sum_nullables(values: list[int | None]) -> int | None:
+    """Helper function to sum nullable values. Handles the following scenarios:
+        - All values are ints: returns sum of values
+        - Some values are ints, some are `None`: Returns sum of non-None values
+        - All values are are `None`: Returns `None`
+
+    :param values: The list of values to sum
+    :returns: The sum of all non-None values, or `None` (see above for details)
+    """
+    running_sum: int | None = None
+    for value in values:
+        if value is not None:
+            running_sum = value if running_sum is None else running_sum + value
+    return running_sum
 
 
 @pytest.fixture(scope="module")
